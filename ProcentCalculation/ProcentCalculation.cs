99908320f1bbde29296b1573cf3e9e9249188454
Pyrkin <apurkin97@gmail.com>
2026-03-27 21:57:51 +0300

    complete lib

---
+++ ProcentCalculation/ProcentCalculation.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcentCalculation
+{
+    public static class ProcentCalculation
+    {
+        //нахождение процента от числа
+        public static double GetProcentByNumber(double number, double procent)
+        {
+            try
+            {
+                return number * procent * 0.01;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return -1;
+            }
+            
+        }
+
+        // исзодное число по проценту и значению
+        public static double GetNumberByProcent(double procent, double NumberOfProcent)
+        {
+            try
+            {
+                return (NumberOfProcent / procent) * 100;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return -1;
+            }
+
+        }
+
+        //процент, который первое число составляет от второго
+        public static double GetProcentOccourencebyNumbers(double valB, double valA)
+        {
+            try
+            {
+                return (valA / valB) * 100;
+            }
+            catch (Exception e)
+            { 
+                Console.Write(e.Message);
+                return -1;
+            }
+
+            
+        }
+    }
+}
